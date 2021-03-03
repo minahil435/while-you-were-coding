67063bbcd3b93698969dbee32675e3d9b765bbfe
Minahil shafique <minahal.shafique@gmail.com>
2021-03-03 13:04:54 -0500

    fun arrays

--- main.js
+++ main.js
@@ -14,13 +14,19 @@ console.log('##### Challenge 1 #####');
 // Console out the numbers from 1-20, but only if they're divisible by 3.
 // Hint: Use `if` and a modulus operation.
 
-
+for (let i = 1; i < 21 ; i++){
+    if (i % 3 === 0){
+    console.log(i)}
+}
 
 console.log('##### Challenge 2 #####');
 // 2.
 //
 // Console out the numbers from 1-20. BACKWARDS.
 
+for (let i = 20; i > 0 ; i--){
+    console.log(i)
+}
 
 
 console.log('##### Challenge 3 #####');
@@ -29,6 +35,9 @@ console.log('##### Challenge 3 #####');
 // For the string 'Boy howdy am I good at this!', console out the letters
 // individually.
 let string3 = 'Boy howdy am I good at this!';
+for (const chars of string3){
+    console.log(chars)
+}
 
 
 
@@ -38,8 +47,9 @@ console.log('##### Challenge 4 #####');
 // For the string, 'And getting better every day.', starting with the second
 // character, print out every third character.
 let string4 = 'And getting better every day.';
-
-
+for (let i = 1; i < string4.length ; i = i + 3){
+    console.log(string4[i])
+}
 
 console.log('##### Challenge 5 #####');
 // 5. 
@@ -47,6 +57,9 @@ console.log('##### Challenge 5 #####');
 // For the string 'Am I the best?', console out the letters individually, AND
 // UPPERCASED.
 let string5 = 'Am I the best?';
+for (const chars of string5){
+    console.log(chars.toUpperCase())
+}
 
 
 
@@ -58,6 +71,19 @@ console.log('##### Challenge 6 #####');
 let string6 = 'Whoa, I am the best!';
 
 
+let newarr = string6.split(" ")
+let string6Changed = []
+for (const words of  newarr){
+    if (words === 'am'){
+        string6Changed.push('AM')
+    }
+    else{
+        string6Changed.push(words)
+    }
+}
+console.log(string6Changed.join(" "));
+
+
 
 console.log('##### Challenge 7 #####');
 // 7.
@@ -67,6 +93,9 @@ console.log('##### Challenge 7 #####');
 // `.`, followed by `s`, followed by `d`, and so on.
 let string7 = 'I am become death, destroyer of worlds.';
 
+for (let i = (string7.length) - 1 ; i > -1 ; i--){
+    console.log(string7[i]);
+}
 
 
 console.log('##### Challenge 8 #####');
@@ -81,6 +110,12 @@ const names = 'Alex|Mesuara|Brian|Chanel|Matt|Jeremy B.|Jose|Andy|Dustin|Joshua|
 // You'll know you're at a new person's name when you hit the pipe character.
 
 // Don't print the pipes!
+
+let newNames = names.split("|");
+
+for (const name of newNames){
+    console.log('Here today is: ' + name);
+}
 
 
 
